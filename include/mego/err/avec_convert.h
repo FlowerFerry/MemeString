3f207c5c05eb69e8a1fa084301fd3c00e407f1a9
FlowerFerry <422040095@qq.com>
2023-09-04 17:52:30 +0800

    -

---
+++ include/mego/err/avec_convert.h
@@ -0,0 +1,18 @@
+
+#ifndef MG_ERR_AVEC_CONVERT_H_INCLUDED
+#define MG_ERR_AVEC_CONVERT_H_INCLUDED
+
+#include <mego/err/ec.h>
+#include <libavutil/error.h>
+
+inline mgec_t mgec__from_averr(int _err)
+{
+	if (_err == 0)
+		return 0;
+	
+    switch (_err) {
+    default:                return mgec__from_posix_err(AVUNERROR(_err));
+    }
+}
+
+#endif // !MG_ERR_AVEC_CONVERT_H_INCLUDED
