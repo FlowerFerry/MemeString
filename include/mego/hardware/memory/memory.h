ada511af740fcf5cbcf5f31d7d7fd14679fce342
FlowerFerry <422040095@qq.com>
2023-12-07 16:18:07 +0800

    -

---
+++ include/mego/hardware/memory/memory.h
@@ -0,0 +1,10 @@
+
+#ifndef MGHW_MEMORY_H_INCLUDED
+#define MGHW_MEMORY_H_INCLUDED
+
+#include "memory_def.h"
+// #ifndef MMU__IMPL_SEPARATE
+#include "memory_simpl.h"
+// #endif
+
+#endif // !MGHW_MEMORY_H_INCLUDED
---
+++ include/mego/hardware/memory/memory_def.h
@@ -0,0 +1,27 @@
+
+#ifndef MGHW_MEMORY_DEF_H_INCLUDED
+#define MGHW_MEMORY_DEF_H_INCLUDED
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cppplusplus
+
+struct mghw_memory_status
+{
+    uint32_t st_size;
+    uint32_t load_physical;
+    uint64_t total_physical;
+    uint64_t available_physical;
+    uint64_t total_virtual;
+    uint64_t available_virtual;
+};
+
+inline int mghw_get_memory_status(struct mghw_memory_status *status);
+
+#ifdef __cplusplus
+}
+#endif // __cppplusplus
+
+#endif // !MGHW_MEMORY_DEF_H_INCLUDED
