fb1ca870f1856f1f4cadb521a87e95c2ffc369c7
FlowerFerry <422040095@qq.com>
2023-12-13 18:16:22 +0800

    -

--- xmake.lua
+++ xmake.lua
@@ -27,7 +27,9 @@ target("meme_string")
     --     add_ldflags("-Wl,-rpath,$ORIGIN", {force = true})
     -- end
 
-    if string.find(get_config("cxx"), "g++") or string.find(get_config("cc"), "gcc") then
-        add_cxflags("-fasm")
-        add_ldflags("-fasm")
+    if string.find(get_config("arch"), "x64") or string.find(get_config("arch"), "x86_64") or string.find(get_config("arch"), "amd64") then
+        if string.find(get_config("cxx"), "g++") or string.find(get_config("cc"), "gcc") then
+            add_cxflags("-fasm")
+            add_ldflags("-fasm")
+        end
     end
