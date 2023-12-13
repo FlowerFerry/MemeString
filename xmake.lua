28b4a23f9ccaa24d918cdd3b12da823f41a3d357
FlowerFerry <422040095@qq.com>
2023-12-13 18:58:40 +0800

    -

--- xmake.lua
+++ xmake.lua
@@ -27,13 +27,13 @@ target("meme_string")
     --     add_ldflags("-Wl,-rpath,$ORIGIN", {force = true})
     -- end
 
-    if (string.find(get_config("arch"), "x64") ~= nil or string.find(get_config("arch"), "x86_64") ~= nil or string.find(get_config("arch"), "amd64") ~= nil) 
-    then
-        local xxComp = get_config("cxx")
-        local ccComp = get_config("cc")
-        if ((xxComp ~= nil and string.find(xxComp, "g++") ~= nil) or (ccComp ~= nil and string.find(ccComp, "gcc") ~= nil)) 
-        then
-            add_cflags("-S", {force = true})
-            -- add_ldflags("-S")
-        end
-    end
+    -- if (string.find(get_config("arch"), "x64") ~= nil or string.find(get_config("arch"), "x86_64") ~= nil or string.find(get_config("arch"), "amd64") ~= nil)
+    -- then
+    --     local xxComp = get_config("cxx")
+    --     local ccComp = get_config("cc")
+    --     if ((xxComp ~= nil and string.find(xxComp, "g++") ~= nil) or (ccComp ~= nil and string.find(ccComp, "gcc") ~= nil))
+    --     then
+    --         add_cflags("-S", {force = true})
+    --         -- add_ldflags("-S")
+    --     end
+    -- end
