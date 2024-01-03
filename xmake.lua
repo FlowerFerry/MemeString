835ff78ce1c76fc00de819b86f46432c15f09e54
FlowerFerry <422040095@qq.com>
2024-01-03 16:49:37 +0800

    -

--- xmake.lua
+++ xmake.lua
@@ -7,33 +7,20 @@ set_version("0.0.1", {build = "%Y%m%d%H%M"})
 
 add_rules("mode.debug", "mode.release")
 
-stdc = "c11"
-set_languages(stdc)
+-- stdc = "c11"
+-- set_languages(stdc)
 
-add_includedirs(
-    "include_private",
-    "3rdparty/cvector/include"
-)
 add_includedirs("include", {public = true})
 
 target("meme_string")
     add_defines("MEME_OPTION__BUILD_SHARED")
+    set_languages("c11")
+    add_includedirs(
+        "include_private",
+        "3rdparty/cvector/include"
+    )
     add_files("src/*.c")
     add_syslinks("pthread", "dl", "rt")
     set_kind("shared")
     set_symbols("hidden")
     add_rpathdirs("$ORIGIN")
-    -- if is_plat("cross", "linux") then
-    --     add_ldflags("-Wl,-rpath,$ORIGIN", {force = true})
-    -- end
-
-    -- if (string.find(get_config("arch"), "x64") ~= nil or string.find(get_config("arch"), "x86_64") ~= nil or string.find(get_config("arch"), "amd64") ~= nil)
-    -- then
-    --     local xxComp = get_config("cxx")
-    --     local ccComp = get_config("cc")
-    --     if ((xxComp ~= nil and string.find(xxComp, "g++") ~= nil) or (ccComp ~= nil and string.find(ccComp, "gcc") ~= nil))
-    --     then
-    --         add_cflags("-S", {force = true})
-    --         -- add_ldflags("-S")
-    --     end
-    -- end
