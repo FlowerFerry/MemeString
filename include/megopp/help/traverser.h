b66d24d8061bed144a43af51e283a5cbddeff563
FlowerFerry <422040095@qq.com>
2024-01-20 17:22:15 +0800

    -

--- include/megopp/help/traverser.h
+++ include/megopp/help/traverser.h
@@ -2,53 +2,84 @@
 #ifndef MGPP_HELP_TRAVERSER_H_INCLUDED
 #define MGPP_HELP_TRAVERSER_H_INCLUDED
 
+#include <type_traits>
 #include <optional>
+#include <megopp/util/scope_cleanup.h>
+#include <megopp/help/iterator.h>
 
 namespace mgpp {
 namespace help {
 
-    template <typename _Container, typename _SharedLocker, typename _Pred>
-    std::optional<typename _Container::key_type> foreach_with_small_step(
-        _SharedLocker& _locker,
-        const _Container& _container, 
-        const std::optional<typename _Container::key_type>& _begin, 
-        size_t _count,
-        _Pred _pred)
+    template <typename _Container, typename _SharedLocker, typename _Ty, typename _Pred>
+    inline void foreach_with_small_step
+        (
+            _SharedLocker& _locker,
+            const _Container& _container, 
+            const std::optional<_Ty>& _begin,
+            size_t _count,
+            std::optional<_Ty>& _out,
+            _Pred _pred
+        )
     {
-        if (!_begin.has_value()) {
-            if (_locker.owns_lock())
-                _locker.unlock();
-            auto it = _container.find(*_begin);
-            if (it == _container.end())
-            {
-                return std::nullopt;
+        auto has_lock = _locker.owns_lock();
+        MEGOPP_UTIL__ON_SCOPE_CLEANUP([&]() {
+            if (has_lock) {
+                if (!_locker.owns_lock())
+                    _locker.lock();
+                
             }
             else {
-                auto end = it;
-                std::advance(end, _count);
-                for (; it != end; ++it)
-                {
-                    if (!_pred(*it))
-                    {
-                        return std::nullopt;
-                    }
-                }
-                return *end;
+                if (_locker.owns_lock())
+                    _locker.unlock();
             }
+        });
+
+        if (!has_lock)
+            _locker.lock();
+        _Container::const_iterator it;
+        if (!_begin.has_value()) {
+            auto& container = const_cast<_Container&>(_container);
+            it = container.find(mgpp::help::iter_key(*_begin));
+            if (it == container.end())
+            {
+                _out = std::nullopt;
+                return;
+            }
+            
         }
         else {
-            auto it = _container.begin();
-            auto end = it;
-            std::advance(end, _count);
-            for (; it != end; ++it)
-            {
-                if (!_pred(*it))
-                {
-                    return std::nullopt;
-                }
-            }
-            return *end;
+            it = _container.begin();
         }
+        auto end = it;
+        std::advance(end, _count);
+        for (; it != end; ++it)
+        {
+            if (!_pred(*it)) {
+                if (++it == _container.end())
+                {
+                    _out = std::nullopt;
+                    return;
+                }
+                
+                if constexpr (std::is_same<_Ty, typename _Container::key_type>::value)
+                    _out = it->first;
+                else {
+                    _out = std::make_pair(it->first, it->second);
+                }
+                return;
+            }
+        }
+
+        if (end == _container.end())
+        {
+            _out = std::nullopt;
+            return;
+        }
+        
+        if constexpr (std::is_same<_Ty, typename _Container::key_type>::value)
+            _out = end->first;
+        else
+            _out = std::make_pair(end->first, end->second);
     }
 
 }
