126f186c2b240c189a9e9741f8215bdb24e87538
FlowerFerry <422040095@qq.com>
2024-03-08 16:34:12 +0800

    -

---
+++ include/memepp/convert/std/string_view.hpp
@@ -0,0 +1,45 @@
+
+#ifndef MEMEPP_CONVERT_STRING_VIEW_HPP_INCLUDED
+#define MEMEPP_CONVERT_STRING_VIEW_HPP_INCLUDED
+
+#include "meme/string.h"
+#include "memepp/string.hpp"
+#include "memepp/string_view.hpp"
+#include "memepp/convert/common_def.hpp"
+
+#include <string_view>
+
+namespace memepp {
+	
+	inline memepp::string from(const std::string_view& _s)
+	{
+		return memepp::string(_s.data(), static_cast<mmint_t>(_s.size()));
+	}
+	
+	inline memepp::string_view view(const std::string_view& _s)
+	{
+		return memepp::string_view{ _s.data(), static_cast<mmint_t>(_s.size()) };
+	}
+
+	template<>
+	inline std::string to<std::string_view>(const memepp::string& _s)
+	{
+		return std::string{ _s.data(), static_cast<size_t>(_s.size()) };
+	}
+
+	template<>
+	inline std::string to<std::string_view>(const memepp::string_view& _sv)
+	{
+		return std::string{ _sv.data(), static_cast<size_t>(_sv.size()) };
+	}
+	
+}
+
+inline memepp::string mm_from(const std::string_view& _s)
+{
+	return memepp::from(_s);
+}
+
+#include <memepp/convert/common.hpp>
+
+#endif // !MEMEPP_CONVERT_STRING_VIEW_HPP_INCLUDED
