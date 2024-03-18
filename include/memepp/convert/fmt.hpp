1f06342c29da1c0e9c12c2686aa889540cceff83
FlowerFerry <422040095@qq.com>
2024-03-18 20:07:27 +0800

    -

--- include/memepp/convert/fmt.hpp
+++ include/memepp/convert/fmt.hpp
@@ -25,4 +25,25 @@ struct fmt::formatter<memepp::string_view> : fmt::formatter<fmt::string_view>
 		return formatter<string_view>::format({ _s.data(), static_cast<size_t>(_s.size()) }, ctx);
 	}
 };
+
+namespace memepp  {
+namespace convert {
+		
+	template<>
+	struct into<::fmt::string_view>
+	{
+		static _Ty into_object(const memepp::string& _v)
+		{
+			return fmt::string_view{ _v.data(), static_cast<size_t>(_v.size()) };
+		}
+		
+		static _Ty into_object(const memepp::string_view& _v)
+		{
+            return fmt::string_view{ _v.data(), static_cast<size_t>(_v.size()) };
+		}
+	};
+
+}
+}
+
 #endif // !MEMEPP_CONVERT_FMT_HPP_INCLUDED
