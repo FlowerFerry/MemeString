a7282e9efe42981d2047cb376e8749ed73823e8e
FlowerFerry <422040095@qq.com>
2024-03-28 09:33:53 +0800

    -

--- include/memepp/convert/toml_ex.hpp
+++ include/memepp/convert/toml_ex.hpp
@@ -4,14 +4,50 @@
 
 #include <memepp/string_def.hpp>
 #include <memepp/string_view_def.hpp>
+#include <memepp/convert/common_def.hpp>
 #include <toml/value.hpp>
 #include "./std/string.hpp"
+
+#include <string_view>
 
 namespace memepp  {
 namespace convert {
 
-    
+    template<>
+	struct from<::toml::string>
+    {
+		static memepp::string from_object(const ::toml::string& _v)
+		{
+            return mm_from(static_cast<const std::string&>(_v));
+		}
+		static memepp::string from_object(::toml::string&& _v)
+		{
+            return mm_from(static_cast<std::string&&>(_v));
+		}
+	};
 
+	template<>
+	struct view<::toml::string>
+	{
+		static memepp::string_view view_object(const ::toml::string& _v)
+		{
+            return mm_view(static_cast<const std::string&>(_v));
+		}
+	};
+
+    template<>
+	struct into<::toml::string>
+	{
+		static ::toml::string into_object(const memepp::string& _v)
+		{
+            return std::string_view{ _v.data(), static_cast<size_t>(_v.size()) };
+		}
+		static ::toml::string into_object(const memepp::string_view& _v)
+		{
+            return std::string_view{ _v.data(), static_cast<size_t>(_v.size()) };
+		}
+	};
+    
 }
 
     inline string from_toml11_table(
