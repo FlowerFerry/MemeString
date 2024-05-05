1c21eebf9056babcd15281f2d9146a8172fd828d
FlowerFerry <422040095@qq.com>
2024-05-06 00:21:14 +0800

    -

---
+++ include/megopp/util/os/win/enum_usn_record.h
@@ -0,0 +1,43 @@
+
+#ifndef MEGOPP_UTIL_OS_WIN_ENUM_USN_RECORD_H_INCLUDED
+#define MEGOPP_UTIL_OS_WIN_ENUM_USN_RECORD_H_INCLUDED
+
+#include <mego/predef/os/windows.h>
+#include <stdint.h>
+
+#if MG_OS__WIN_AVAIL
+#include <winioctl.h>
+#endif 
+
+#include <type_traits>
+
+namespace mgpp {
+namespace os   {
+namespace win  {
+#if MG_OS__WIN_AVAIL
+
+template<typename _Fn,
+    typename = std::enable_if_t<std::is_invocable_v<_Fn, const USN_RECORD_UNION&>>>
+inline void enum_usn_record_buffer(const uint8_t* _buffer, size_t _size, _Fn&& _fn)
+{
+    auto p = buffer + sizeof(USN);
+    auto end = buffer + size;
+    while (p < end) {
+        auto record = reinterpret_cast<const USN_RECORD_UNION*>(p);
+        if constexpr (std::is_same_v<std::invoke_result_t<_Fn, const USN_RECORD_UNION&>, bool>) {
+            if (!_fn(*record))
+                break;
+        }
+        else
+            _fn(*record);
+        
+        p += record->Header.RecordLength;
+    }
+}
+
+#endif
+}
+}
+}   
+
+#endif // !MEGOPP_UTIL_OS_WIN_ENUM_USN_RECORD_H_INCLUDED
