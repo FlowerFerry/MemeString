098030e4f88169127f31928dedafb0e89ab3a41b
FlowerFerry <422040095@qq.com>
2024-05-08 03:04:05 +0800

    -

---
+++ include/megopp/memory/emtx_cow_ptr.h
@@ -0,0 +1,252 @@
+
+#ifndef MEGOPP_MEMORY_COW_PTR_H_INCLUDED
+#define MEGOPP_MEMORY_COW_PTR_H_INCLUDED
+
+#include <megopp/help/null_mutex.h>
+#include <megopp/util/scope_locker.h>
+
+#include <memory>
+#include <mutex>
+#include <shared_mutex>
+
+namespace mgpp {
+namespace mem {
+
+template <
+    typename _Ty, 
+    typename _WriteMutex = std::mutex, 
+    typename _GenrcMutex = std::shared_mutex>
+class emtx_cow_ptr 
+{
+public:
+
+    emtx_cow_ptr() noexcept
+        : ptr_()
+    {    
+    }
+
+    emtx_cow_ptr(std::nullptr_t) noexcept
+        : ptr_(nullptr)
+    {}
+
+    template <typename _Derived>
+    emtx_cow_ptr(_Derived* _ptr)
+        : ptr_(_ptr)
+    {}
+
+    template <typename _Derived, typename _Deleter>
+    emtx_cow_ptr(_Derived* _ptr, _Deleter _deleter)
+        : ptr_(_ptr, _deleter)
+    {}
+
+    template<typename _Deleter>
+    emtx_cow_ptr(std::nullptr_t, _Deleter _deleter)
+        : ptr_(nullptr, _deleter)
+    {}
+
+    template<typename _Derived, typename _Deleter, typename _Alloc>
+    emtx_cow_ptr(_Derived* _ptr, _Deleter _deleter, _Alloc _alloc)
+        : ptr_(_ptr, _deleter, _alloc)
+    {}
+
+    template<typename _Deleter, typename _Alloc>
+    emtx_cow_ptr(std::nullptr_t, _Deleter _deleter, _Alloc _alloc)
+        : ptr_(nullptr, _deleter, _alloc)
+    {}
+
+    emtx_cow_ptr(const emtx_cow_ptr& _other)
+        : ptr_(_other.read())
+    {}
+
+    emtx_cow_ptr(emtx_cow_ptr&& _other)
+        : ptr_(_other.release())
+    {}
+
+    template<typename _Derived>
+    emtx_cow_ptr(const std::shared_ptr<_Derived>& _ptr) noexcept
+        : ptr_(_ptr)
+    {}
+
+    template<typename _Derived>
+    emtx_cow_ptr(std::shared_ptr<_Derived>&& _ptr) noexcept
+        : ptr_(std::move(_ptr))
+    {}
+
+    template<typename _Derived>
+    emtx_cow_ptr(const std::weak_ptr<_Derived>& _ptr)
+        : ptr_(_ptr)
+    {}
+
+    template<typename _Derived, typename _Deleter>
+    emtx_cow_ptr(std::unique_ptr<_Derived, _Deleter>&& _ptr)
+        : ptr_(std::move(_ptr))
+    {}
+
+    template<
+        typename = std::enable_if_t<
+            std::is_same<_GenrcMutex, megopp::help::null_mutex>::value>>
+    inline emtx_cow_ptr& assign(const emtx_cow_ptr& _other)
+    {
+        if (this != &_other) {
+            ptr_ = _other.read();
+        }
+        return *this;
+    }
+
+    inline emtx_cow_ptr& assign(const emtx_cow_ptr& _other, std::unique_lock<_GenrcMutex>& _locker)
+    {
+        if (this != &_other) {
+            util::scope_unique_locker<_GenrcMutex> locker(_locker);
+            ptr_ = _other.ptr_;
+        }
+        return *this;
+    }
+
+    template<
+        typename = std::enable_if_t<
+            std::is_same<_GenrcMutex, megopp::help::null_mutex>::value>>
+    inline emtx_cow_ptr& assign(emtx_cow_ptr&& _other)
+    {
+        if (this != &_other) {
+            ptr_ = _other.release();
+        }
+        return *this;
+    }
+
+    inline emtx_cow_ptr& assign(emtx_cow_ptr&& _other, std::unique_lock<_GenrcMutex>& _locker)
+    {
+        if (this != &_other) {
+            util::scope_unique_locker<_GenrcMutex> locker(_locker);
+            ptr_ = _other.release(_locker);
+        }
+        return *this;
+    }
+
+    template<typename _Derived>
+    inline emtx_cow_ptr& assign(const std::shared_ptr<_Derived>& _ptr, std::unique_lock<_GenrcMutex>& _locker)
+    {
+        util::scope_unique_locker<_GenrcMutex> locker(_locker);
+        ptr_ = _ptr;
+        return *this;
+    }
+
+    template<typename _Derived>
+    inline emtx_cow_ptr& assign(std::shared_ptr<_Derived>&& _ptr, std::unique_lock<_GenrcMutex>& _locker)
+    {
+        util::scope_unique_locker<_GenrcMutex> locker(_locker);
+        ptr_ = std::move(_ptr);
+        return *this;
+    }
+
+    template<typename _Derived, typename _Deleter>
+    inline emtx_cow_ptr& assign(std::unique_ptr<_Derived, _Deleter>&& _ptr, std::unique_lock<_GenrcMutex>& _locker)
+    {
+        util::scope_unique_locker<_GenrcMutex> locker(_locker);
+        ptr_ = std::move(_ptr);
+        return *this;
+    }
+
+    template<
+        typename = std::enable_if_t<
+            std::is_same<_GenrcMutex, megopp::help::null_mutex>::value>>
+    inline std::shared_ptr<const _Ty> read() const
+    {
+        return ptr_;
+    }
+
+    inline std::shared_ptr<const _Ty> read(std::shared_lock<_GenrcMutex>& _locker) const
+    {
+        util::scope_shared_locker<_GenrcMutex> locker(_locker);
+        return ptr_;
+    }
+
+    template<typename _Fn, 
+        typename = std::enable_if_t<std::is_invocable_v<_Fn, std::shared_ptr<_Ty>&>>>
+    inline std::shared_ptr<const _Ty> write(
+        std::unique_lock<_WriteMutex>& _write_locker, 
+        util::rw_lock<_GenrcMutex>& _genrc_locker,
+        _Fn&& _fn)
+    {
+        util::scope_unique_locker write_locker(_write_locker);
+
+        _genrc_locker.lock_shared();
+        auto old_ptr = ptr_;
+        _genrc_locker.unlock();
+        auto new_ptr = std::make_shared<_Ty>(*old_ptr);
+
+        if constexpr (std::is_same_v<std::invoke_result_t<_Fn, std::shared_ptr<_Ty>&>, bool>)
+        {
+            if (!_fn(new_ptr))
+                return old_ptr;
+        }
+        else {
+            _fn(new_ptr);
+        }
+
+        _genrc_locker.lock();
+        ptr_ = new_ptr;
+        _genrc_locker.unlock();
+        _write_locker.unlock();
+        return new_ptr;
+    }
+
+    template<typename _CondFn, typename _Fn, 
+        typename = std::enable_if_t<std::is_invocable_v<_Fn, std::shared_ptr<_Ty>&>>>
+    inline std::shared_ptr<const _Ty> write_if(
+        std::unique_lock<_WriteMutex>& _write_locker, 
+        util::rw_lock<_GenrcMutex>& _genrc_locker,
+        _CondFn&& _cond_fn, _Fn&& _fn)
+    {
+        _genrc_locker.lock_shared();
+        auto old_ptr = ptr_;
+        _genrc_locker.unlock();
+
+        if constexpr (std::is_same_v<std::invoke_result_t<_CondFn, const std::shared_ptr<_Ty>&>, bool>)
+        {
+            if (!_cond_fn(old_ptr))
+                return old_ptr;
+        }
+
+        util::scope_unique_locker write_locker(_write_locker);
+        old_ptr = read();
+        auto new_ptr = std::make_shared<_Ty>(*old_ptr);
+
+        if constexpr (std::is_same_v<std::invoke_result_t<_Fn, std::shared_ptr<_Ty>&>, bool>)
+        {
+            if (!_fn(new_ptr))
+                return old_ptr;
+        }
+        else {
+            _fn(new_ptr);
+        }
+
+        _genrc_locker.lock();
+        ptr_ = new_ptr;
+        _genrc_locker.unlock();
+        _write_locker.unlock();
+        return new_ptr;
+    }
+
+    template<
+        typename = std::enable_if_t<
+            std::is_same<_GenrcMutex, megopp::help::null_mutex>::value>>
+    inline std::shared_ptr<_Ty> release()
+    {
+        return std::move(ptr_);
+    }
+
+    inline std::shared_ptr<_Ty> release(std::unique_lock<_GenrcMutex>& _locker)
+    {
+        util::scope_unique_locker<_GenrcMutex> locker(_locker);
+        return std::move(ptr_);
+    }
+
+private:
+
+    std::shared_ptr<_Ty> ptr_;
+};
+
+}
+}
+
+#endif // !MEGOPP_MEMORY_COW_PTR_H_INCLUDED
--- include/megopp/util/scope_locker.h
+++ include/megopp/util/scope_locker.h
@@ -11,6 +11,8 @@ namespace util {
 template <typename _Mtx>
 class scope_unique_locker {
 public:
+    using mutex_type = _Mtx;
+
     explicit scope_unique_locker(std::unique_lock<_Mtx>& _locker) 
         : locker_(_locker), src_owns_lock_(_locker.owns_lock())
     {
@@ -58,6 +60,8 @@ private:
 template <typename _Mtx>
 class scope_shared_locker {
 public:
+    using mutex_type = _Mtx;
+
     explicit scope_shared_locker(std::shared_lock<_Mtx>& _locker) 
         : locker_(_locker), src_owns_lock_(_locker.owns_lock())
     {
@@ -102,10 +106,98 @@ private:
     bool src_owns_lock_;
 };
 
+template <typename _Mtx>
+class rw_lock {
+
+public:
+    using mutex_type = _Mtx;
+
+    explicit rw_lock(_Mtx& _mtx) 
+        : mtx_(_mtx), owns_lock_(false), is_shared_(false)
+    {
+        mtx_.lock();
+        owns_lock_ = true;
+    }
+
+    explicit rw_lock(_Mtx& _mtx, std::defer_lock_t) 
+        : mtx_(_mtx), owns_lock_(false), is_shared_(false)
+    {
+    }
+
+    explicit rw_lock(_Mtx& _mtx, std::try_to_lock_t) 
+        : mtx_(_mtx), owns_lock_(mtx_.try_lock()), is_shared_(false)
+    {
+    }
+
+    explicit rw_lock(_Mtx& _mtx, std::adopt_lock_t) 
+        : mtx_(_mtx), owns_lock_(true), is_shared_(false)
+    {
+    }
+
+    ~rw_lock() {
+        if (owns_lock_) {
+            if (is_shared_) {
+                mtx_.unlock_shared();
+            }
+            else {
+                mtx_.unlock();
+            }
+        }
+    }
+
+    void lock() {
+        if (!owns_lock_) {
+            mtx_.lock();
+            owns_lock_ = true;
+        }
+    }
+
+    void unlock() {
+        if (owns_lock_) {
+            if (is_shared_) {
+                mtx_.unlock_shared();
+                is_shared_ = false;
+            }
+            else {
+                mtx_.unlock();
+            }
+            owns_lock_ = false;
+        }
+    }
+
+    bool try_lock() {
+        if (!owns_lock_) {
+            owns_lock_ = mtx_.try_lock();
+        }
+        return owns_lock_;
+    }
+
+    void lock_shared() {
+        if (!owns_lock_) {
+            mtx_.lock_shared();
+            is_shared_ = true;
+        }
+    }
+
+    bool try_lock_shared() {
+        if (!owns_lock_) {
+            is_shared_ = mtx_.try_lock_shared();
+        }
+        return is_shared_;
+    }
+
+private:
+    _Mtx& mtx_;
+    bool owns_lock_;
+    bool is_shared_;
+};
+
 template <typename _Locker>
 class scope_locker {
 
 public:
+    using mutex_type = _Locker::mutex_type;
+
     explicit scope_locker(_Locker& _locker) 
         : locker_(_locker), src_owns_lock_(_locker.owns_lock())
     {
