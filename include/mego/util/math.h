1bf9712f6e2d4f6bbe8f88695a4b1abe7a83e799
FlowerFerry <422040095@qq.com>
2024-07-31 15:33:59 +0800

    -

---
+++ include/mego/util/math.h
@@ -0,0 +1,14 @@
+
+#ifndef MEGO_UTIL_MATH_H_INCLUDED
+#define MEGO_UTIL_MATH_H_INCLUDED
+
+#ifndef MGU_MATH__MAX
+#define MGU_MATH__MAX(a, b) (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef MGU_MATH__MIN
+#define MGU_MATH__MIN(a, b) (((a) < (b)) ? (a) : (b))
+#endif
+
+
+#endif // !MEGO_UTIL_MATH_H_INCLUDED
