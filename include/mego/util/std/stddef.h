905e6f6082786b0f403b44373fb7187bfc5d873c
FlowerFerry <422040095@qq.com>
2024-08-02 10:02:39 +0800

    -

---
+++ include/mego/util/std/stddef.h
@@ -0,0 +1,12 @@
+
+#ifndef MEGO_UTIL_STD_STDDEF_H_INCLUDED
+#define MEGO_UTIL_STD_STDDEF_H_INCLUDED
+
+#include <stddef.h>
+
+#ifndef RSIZE_MAX
+#define RSIZE_MAX (SIZE_MAX >> 1)
+typedef size_t rsize_t;
+#endif
+
+#endif // !MEGO_UTIL_STD_STDDEF_H_INCLUDED
