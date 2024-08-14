e3dc07c937b40b51ea2a5dcfe53e7cbd5c6eafc0
FlowerFerry <422040095@qq.com>
2024-08-14 09:41:32 +0800

    -

--- include/mego/thrd/spinlock.h
+++ include/mego/thrd/spinlock.h
@@ -36,47 +36,4 @@ MG_CAPI_INLINE void mgthrd_spinlock_unlock(mgthrd_spinlock_t*_lock)
     mgu_atomic_flag_clear(&_lock->locked);
 }
 
-//MG_CAPI_INLINE void mgthrd_spinlock_lock_all(mgthrd_spinlock_t*_locks[], size_t _n)
-//{
-//    size_t locked_count = 0;
-//    mgthrd_spinwait_t spinwait;
-//    mgthrd_spinwait_reset(&spinwait);
-//    while(locked_count < _n) 
-//    {
-//        for(size_t i = 0; i < _n; ++i) 
-//        {
-//            if (_locks[i] == NULL)
-//            {
-//                ++locked_count;
-//                continue;
-//            }
-//
-//            if(!mgthrd_spinlock_trylock(_locks[i]))
-//            {
-//                for (size_t j = 0; j < i; ++j) 
-//                {
-//                    if (_locks[j] == NULL)
-//                        continue;
-//                    mgthrd_spinlock_unlock(_locks[j]);
-//                }
-//                locked_count = 0;
-//                mgthrd_spinwait_once(&spinwait);
-//                break;
-//            }
-//            ++locked_count;
-//        }
-//    }
-//}
-//
-//MG_CAPI_INLINE void mgthrd_spinlock_unlock_all(mgthrd_spinlock_t*_locks[], size_t _n)
-//{
-//    for(size_t i = 0; i < _n; ++i) 
-//    {
-//        if (_locks[i] == NULL)
-//            continue;
-//        mgthrd_spinlock_unlock(_locks[i]);
-//    }
-//    
-//}
-
 #endif // !MEGO_THRD_SPINLOCK_H_INCLUDED
