935ba47147a183da2840f95afb7ba61d6cdd339e
FlowerFerry <flower_ferry@outlook.com>
2024-09-20 18:27:22 +0800

    -

--- include/megopp/util/os/win/enum_usn_record.h
+++ include/megopp/util/os/win/enum_usn_record.h
@@ -20,18 +20,22 @@ template<typename _Fn,
     typename = std::enable_if_t<std::is_invocable_v<_Fn, const USN_RECORD_UNION&>>>
 inline void enum_usn_record_buffer(const uint8_t* _buffer, size_t _size, _Fn&& _fn)
 {
-    auto p = _buffer + sizeof(USN);
+    auto ptr = _buffer + sizeof(USN);
     auto end = _buffer + _size;
-    while (p < end) {
-        auto record = reinterpret_cast<const USN_RECORD_UNION*>(p);
-        if constexpr (std::is_same_v<std::invoke_result_t<_Fn, const USN_RECORD_UNION&>, bool>) {
+    const USN_RECORD_UNION* record = reinterpret_cast<const USN_RECORD_UNION*>(ptr);
+    while (ptr + record->Header.RecordLength < end) 
+    {
+        record = reinterpret_cast<const USN_RECORD_UNION*>(ptr);
+
+        if constexpr (std::is_same_v<std::invoke_result_t<_Fn, const USN_RECORD_UNION&>, bool>) 
+        {
             if (!_fn(*record))
                 break;
         }
         else
             _fn(*record);
         
-        p += record->Header.RecordLength;
+        ptr += record->Header.RecordLength;
     }
 }
 
