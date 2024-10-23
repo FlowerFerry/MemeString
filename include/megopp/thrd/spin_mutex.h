cff94ed48b5ccac4074e07836ff4879805a70aa6
FlowerFerry <flower_ferry@outlook.com>
2024-10-23 15:12:41 +0800

    -

--- include/megopp/thrd/spin_mutex.h
+++ include/megopp/thrd/spin_mutex.h
@@ -3,8 +3,14 @@
 #define MEGOPP_THRD_SPIN_MUTEX_H_INCLUDED
 
 #include <mego/thrd/spinwait.h>
+#include <mego/predef/os/linux.h>
 
 #include <atomic>
+#include <stdexcept>
+
+#if MG_OS__LINUX_AVAIL
+#  include <unistd.h>
+#endif
 
 namespace mgpp {
 namespace thrd {
@@ -22,26 +28,59 @@ struct spin_mutex
 
     inline void lock()
     {
+#if MG_OS__WIN_AVAIL
+        auto tid = GetCurrentThreadId();
+#else
+        auto tid = gettid();
+#endif
+        int64_t expected = -1;
+
         mgthrd_spinwait_t spinwait;
         mgthrd_spinwait_reset(&spinwait);
-        while (locked.test_and_set(std::memory_order_acquire)) 
+        
+        if (locked.load(std::memory_order_acquire) == tid)
+            throw std::logic_error("deadlock detected");
+        
+        while (locked.compare_exchange_weak(expected, tid, std::memory_order_acquire))
         {
+            expected = -1;
             mgthrd_spinwait_once(&spinwait);
         }
+
+        // while (locked.test_and_set(std::memory_order_acquire)) 
+        // {
+        //     mgthrd_spinwait_once(&spinwait);
+        // }
     }
 
     inline bool try_lock()
     {
-        return !locked.test_and_set(std::memory_order_acquire);
+#if MG_OS__WIN_AVAIL
+        auto tid = GetCurrentThreadId();
+#else
+        auto tid = gettid();
+#endif
+        int64_t expected = -1;
+
+        if (locked.load(std::memory_order_acquire) == tid)
+            throw std::logic_error("deadlock detected");
+        
+        return locked.compare_exchange_strong(expected, tid, std::memory_order_acquire);
+
+        // return !locked.test_and_set(std::memory_order_acquire);
     }
 
     inline void unlock()
     {
-        locked.clear(std::memory_order_release);
+        locked.store(-1, std::memory_order_release);
+
+        // locked.clear(std::memory_order_release);
     }
 
 private:
-    std::atomic_flag locked = ATOMIC_FLAG_INIT;
+    std::atomic<int64_t> locked = -1;
+    
+    // std::atomic_flag locked = ATOMIC_FLAG_INIT;
 };
 
 }    
