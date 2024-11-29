e635d607afe54f2a78c361384f89b4ec500d65e9
FlowerFerry <flower_ferry@outlook.com>
2024-11-29 20:39:47 +0800

    -

---
+++ include/megopp/err/convert/std/error_code.h
@@ -0,0 +1,25 @@
+﻿
+#ifndef MEGOPP_ERR_CONVERT_STD_ERROR_CODE_H_INCLUDED
+#define MEGOPP_ERR_CONVERT_STD_ERROR_CODE_H_INCLUDED
+
+#include <mego/err/ec_impl.h>
+
+#include <system_error>
+#include <megopp/err/err.h>
+
+namespace mgpp {
+
+    inline err into_err(const std::error_code& _ec)
+    {
+        if (_ec.category() == std::generic_category())
+            return { mgec__from_posix_err(_ec.value()) };
+
+        if (_ec.category() == std::system_category())
+            return { mgec__from_sys_err(_ec.value()) };
+        
+        return { MGEC__ERR };
+    }
+
+}
+
+#endif // !MEGOPP_ERR_CONVERT_STD_ERROR_CODE_H_INCLUDED
