0f9d74090c2a40bf8dc031c2bbca4b779ac5166a
FlowerFerry <flower_ferry@outlook.com>
2024-12-30 17:49:23 +0800

    -

---
+++ include_private/meme/impl/simd/simd.h
@@ -0,0 +1,214 @@
+﻿
+#ifndef MEME_IMPL_SIMD_SIMD_H_INCLUDED
+#define MEME_IMPL_SIMD_SIMD_H_INCLUDED
+
+#include <meme/impl/simd/simd_fwd.h>
+
+MEME_EXTERN_C_SCOPE_START
+
+struct mmsimd_arith8_hdlr
+{
+    mmsimd_i8_func_t* i8_add_fn;
+    mmsimd_i8_func_t* i8_sub_fn;
+    mmsimd_i8_func_t* i8_mul_fn;
+    mmsimd_i8_func_t* i8_div_fn;
+    mmsimd_u8_func_t* u8_add_fn;
+    mmsimd_u8_func_t* u8_sub_fn;
+    mmsimd_u8_func_t* u8_mul_fn;
+    mmsimd_u8_func_t* u8_div_fn;
+    mmsimd_i8_scalar_func_t* i8_add_scalar_fn;
+    mmsimd_i8_scalar_func_t* i8_sub_scalar_fn;
+    mmsimd_i8_scalar_func_t* i8_mul_scalar_fn;
+    mmsimd_i8_scalar_func_t* i8_div_scalar_fn;
+    mmsimd_u8_scalar_func_t* u8_add_scalar_fn;
+    mmsimd_u8_scalar_func_t* u8_sub_scalar_fn;
+    mmsimd_u8_scalar_func_t* u8_mul_scalar_fn;
+    mmsimd_u8_scalar_func_t* u8_div_scalar_fn;
+};
+
+struct mmsimd_arith16_hdlr
+{
+    mmsimd_i16_func_t* i16_add_fn;
+    mmsimd_i16_func_t* i16_sub_fn;
+    mmsimd_i16_func_t* i16_mul_fn;
+    mmsimd_i16_func_t* i16_div_fn;
+    mmsimd_u16_func_t* u16_add_fn;
+    mmsimd_u16_func_t* u16_sub_fn;
+    mmsimd_u16_func_t* u16_mul_fn;
+    mmsimd_u16_func_t* u16_div_fn;
+    mmsimd_i16_scalar_func_t* i16_add_scalar_fn;
+    mmsimd_i16_scalar_func_t* i16_sub_scalar_fn;
+    mmsimd_i16_scalar_func_t* i16_mul_scalar_fn;
+    mmsimd_i16_scalar_func_t* i16_div_scalar_fn;
+    mmsimd_u16_scalar_func_t* u16_add_scalar_fn;
+    mmsimd_u16_scalar_func_t* u16_sub_scalar_fn;
+    mmsimd_u16_scalar_func_t* u16_mul_scalar_fn;
+    mmsimd_u16_scalar_func_t* u16_div_scalar_fn;
+};
+
+struct mmsimd_arith32_hdlr
+{
+    mmsimd_i32_func_t* i32_add_fn;
+    mmsimd_i32_func_t* i32_sub_fn;
+    mmsimd_i32_func_t* i32_mul_fn;
+    mmsimd_i32_func_t* i32_div_fn;
+    mmsimd_u32_func_t* u32_add_fn;
+    mmsimd_u32_func_t* u32_sub_fn;
+    mmsimd_u32_func_t* u32_mul_fn;
+    mmsimd_u32_func_t* u32_div_fn;
+    mmsimd_i32_scalar_func_t* i32_add_scalar_fn;
+    mmsimd_i32_scalar_func_t* i32_sub_scalar_fn;
+    mmsimd_i32_scalar_func_t* i32_mul_scalar_fn;
+    mmsimd_i32_scalar_func_t* i32_div_scalar_fn;
+    mmsimd_u32_scalar_func_t* u32_add_scalar_fn;
+    mmsimd_u32_scalar_func_t* u32_sub_scalar_fn;
+    mmsimd_u32_scalar_func_t* u32_mul_scalar_fn;
+    mmsimd_u32_scalar_func_t* u32_div_scalar_fn;
+    mmsimd_f32_func_t* f32_add_fn;
+    mmsimd_f32_func_t* f32_sub_fn;
+    mmsimd_f32_func_t* f32_mul_fn;
+    mmsimd_f32_func_t* f32_div_fn;
+    mmsimd_f32_scalar_func_t* f32_add_scalar_fn;
+    mmsimd_f32_scalar_func_t* f32_sub_scalar_fn;
+    mmsimd_f32_scalar_func_t* f32_mul_scalar_fn;
+    mmsimd_f32_scalar_func_t* f32_div_scalar_fn;
+};
+
+struct mmsimd_arith64_hdlr
+{
+    mmsimd_i64_func_t* i64_add_fn;
+    mmsimd_i64_func_t* i64_sub_fn;
+    mmsimd_i64_func_t* i64_mul_fn;
+    mmsimd_i64_func_t* i64_div_fn;
+    mmsimd_u64_func_t* u64_add_fn;
+    mmsimd_u64_func_t* u64_sub_fn;
+    mmsimd_u64_func_t* u64_mul_fn;
+    mmsimd_u64_func_t* u64_div_fn;
+    mmsimd_i64_scalar_func_t* i64_add_scalar_fn;
+    mmsimd_i64_scalar_func_t* i64_sub_scalar_fn;
+    mmsimd_i64_scalar_func_t* i64_mul_scalar_fn;
+    mmsimd_i64_scalar_func_t* i64_div_scalar_fn;
+    mmsimd_u64_scalar_func_t* u64_add_scalar_fn;
+    mmsimd_u64_scalar_func_t* u64_sub_scalar_fn;
+    mmsimd_u64_scalar_func_t* u64_mul_scalar_fn;
+    mmsimd_u64_scalar_func_t* u64_div_scalar_fn;
+    mmsimd_f64_func_t* f64_add_fn;
+    mmsimd_f64_func_t* f64_sub_fn;
+    mmsimd_f64_func_t* f64_mul_fn;
+    mmsimd_f64_func_t* f64_div_fn;
+    mmsimd_f64_scalar_func_t* f64_add_scalar_fn;
+    mmsimd_f64_scalar_func_t* f64_sub_scalar_fn;
+    mmsimd_f64_scalar_func_t* f64_mul_scalar_fn;
+    mmsimd_f64_scalar_func_t* f64_div_scalar_fn;
+
+};
+
+struct mmsimd_conv8_hdlr
+{
+    mmsimd_i8_to_i16_func_t* i8_to_i16_fn;
+    mmsimd_i8_to_u16_func_t* i8_to_u16_fn;
+    mmsimd_i8_to_i32_func_t* i8_to_i32_fn;
+    mmsimd_i8_to_u32_func_t* i8_to_u32_fn;
+    mmsimd_i8_to_i64_func_t* i8_to_i64_fn;
+    mmsimd_i8_to_u64_func_t* i8_to_u64_fn;
+    mmsimd_i8_to_f32_func_t* i8_to_f32_fn;
+    mmsimd_i8_to_f64_func_t* i8_to_f64_fn;
+
+    mmsimd_u8_to_i16_func_t* u8_to_i16_fn;
+    mmsimd_u8_to_u16_func_t* u8_to_u16_fn;
+    mmsimd_u8_to_i32_func_t* u8_to_i32_fn;
+    mmsimd_u8_to_u32_func_t* u8_to_u32_fn;
+    mmsimd_u8_to_i64_func_t* u8_to_i64_fn;
+    mmsimd_u8_to_u64_func_t* u8_to_u64_fn;
+    mmsimd_u8_to_f32_func_t* u8_to_f32_fn;
+    mmsimd_u8_to_f64_func_t* u8_to_f64_fn;
+};
+
+struct mmsimd_conv16_hdlr
+{
+    mmsimd_i16_to_i8_func_t*  i16_to_i8_fn;
+    mmsimd_i16_to_u8_func_t*  i16_to_u8_fn;
+    mmsimd_i16_to_i32_func_t* i16_to_i32_fn;
+    mmsimd_i16_to_u32_func_t* i16_to_u32_fn;
+    mmsimd_i16_to_i64_func_t* i16_to_i64_fn;
+    mmsimd_i16_to_u64_func_t* i16_to_u64_fn;
+    mmsimd_i16_to_f32_func_t* i16_to_f32_fn;
+    mmsimd_i16_to_f64_func_t* i16_to_f64_fn;
+
+    mmsimd_u16_to_i8_func_t*  u16_to_i8_fn;
+    mmsimd_u16_to_u8_func_t*  u16_to_u8_fn;
+    mmsimd_u16_to_i32_func_t* u16_to_i32_fn;
+    mmsimd_u16_to_u32_func_t* u16_to_u32_fn;
+    mmsimd_u16_to_i64_func_t* u16_to_i64_fn;
+    mmsimd_u16_to_u64_func_t* u16_to_u64_fn;
+    mmsimd_u16_to_f32_func_t* u16_to_f32_fn;
+    mmsimd_u16_to_f64_func_t* u16_to_f64_fn;
+};
+
+
+struct mmsimd_conv32_hdlr
+{
+    mmsimd_i32_to_i8_func_t*  i32_to_i8_fn;
+    mmsimd_i32_to_u8_func_t*  i32_to_u8_fn;
+    mmsimd_i32_to_i16_func_t* i32_to_i16_fn;
+    mmsimd_i32_to_u16_func_t* i32_to_u16_fn;
+    mmsimd_i32_to_i64_func_t* i32_to_i64_fn;
+    mmsimd_i32_to_u64_func_t* i32_to_u64_fn;
+    mmsimd_i32_to_f32_func_t* i32_to_f32_fn;
+    mmsimd_i32_to_f64_func_t* i32_to_f64_fn;
+
+    mmsimd_u32_to_i8_func_t*  u32_to_i8_fn;
+    mmsimd_u32_to_u8_func_t*  u32_to_u8_fn;
+    mmsimd_u32_to_i16_func_t* u32_to_i16_fn;
+    mmsimd_u32_to_u16_func_t* u32_to_u16_fn;
+    mmsimd_u32_to_i64_func_t* u32_to_i64_fn;
+    mmsimd_u32_to_u64_func_t* u32_to_u64_fn;
+    mmsimd_u32_to_f32_func_t* u32_to_f32_fn;
+    mmsimd_u32_to_f64_func_t* u32_to_f64_fn;
+
+    mmsimd_f32_to_i8_func_t*  f32_to_i8_fn;
+    mmsimd_f32_to_u8_func_t*  f32_to_u8_fn;
+    mmsimd_f32_to_i16_func_t* f32_to_i16_fn;
+    mmsimd_f32_to_u16_func_t* f32_to_u16_fn;
+    mmsimd_f32_to_i32_func_t* f32_to_i32_fn;
+    mmsimd_f32_to_u32_func_t* f32_to_u32_fn;
+    mmsimd_f32_to_i64_func_t* f32_to_i64_fn;
+    mmsimd_f32_to_u64_func_t* f32_to_u64_fn;
+    mmsimd_f32_to_f64_func_t* f32_to_f64_fn;
+
+};
+
+
+struct mmsimd_conv64_hdlr
+{
+    mmsimd_i64_to_i8_func_t*  i64_to_i8_fn;
+    mmsimd_i64_to_u8_func_t*  i64_to_u8_fn;
+    mmsimd_i64_to_i16_func_t* i64_to_i16_fn;
+    mmsimd_i64_to_u16_func_t* i64_to_u16_fn;
+    mmsimd_i64_to_i32_func_t* i64_to_i32_fn;
+    mmsimd_i64_to_u32_func_t* i64_to_u32_fn;
+    mmsimd_i64_to_f32_func_t* i64_to_f32_fn;
+    mmsimd_i64_to_f64_func_t* i64_to_f64_fn;
+
+    mmsimd_u64_to_i8_func_t*  u64_to_i8_fn;
+    mmsimd_u64_to_u8_func_t*  u64_to_u8_fn;
+    mmsimd_u64_to_i16_func_t* u64_to_i16_fn;
+    mmsimd_u64_to_u16_func_t* u64_to_u16_fn;
+    mmsimd_u64_to_i32_func_t* u64_to_i32_fn;
+    mmsimd_u64_to_u32_func_t* u64_to_u32_fn;
+    mmsimd_u64_to_f32_func_t* u64_to_f32_fn;
+    mmsimd_u64_to_f64_func_t* u64_to_f64_fn;
+
+    mmsimd_f64_to_i8_func_t*  f64_to_i8_fn;
+    mmsimd_f64_to_u8_func_t*  f64_to_u8_fn;
+    mmsimd_f64_to_i16_func_t* f64_to_i16_fn;
+    mmsimd_f64_to_u16_func_t* f64_to_u16_fn;
+    mmsimd_f64_to_i32_func_t* f64_to_i32_fn;
+    mmsimd_f64_to_u32_func_t* f64_to_u32_fn;
+    mmsimd_f64_to_i64_func_t* f64_to_i64_fn;
+    mmsimd_f64_to_u64_func_t* f64_to_u64_fn;
+    mmsimd_f64_to_f32_func_t* f64_to_f32_fn;
+};
+
+MEME_EXTERN_C_SCOPE_ENDED
+#endif // !MEME_IMPL_SIMD_SIMD_H_INCLUDED
---
+++ include_private/meme/impl/simd/simd_fwd.h
@@ -0,0 +1,175 @@
+﻿
+#ifndef MEME_IMPL_SIMD_SIMD_FWD_H_INCLUDED
+#define MEME_IMPL_SIMD_SIMD_FWD_H_INCLUDED
+
+#include <meme/string_fwd.h>
+#include <mego/err/ec.h>
+
+#include <stdint.h>
+
+MEME_EXTERN_C_SCOPE_START
+
+
+struct mmsimd_arith8_hdlr;
+typedef struct mmsimd_arith8_hdlr mmsimd_arith8_hdlr_t;
+
+struct mmsimd_arith16_hdlr;
+typedef struct mmsimd_arith16_hdlr mmsimd_arith16_hdlr_t;
+
+struct mmsimd_arith32_hdlr;
+typedef struct mmsimd_arith32_hdlr mmsimd_arith32_hdlr_t;
+
+struct mmsimd_arith64_hdlr;
+typedef struct mmsimd_arith64_hdlr mmsimd_arith64_hdlr_t;
+
+struct mmsimd_conv8_hdlr;
+typedef struct mmsimd_conv8_hdlr mmsimd_conv8_hdlr_t;
+
+struct mmsimd_conv16_hdlr;
+typedef struct mmsimd_conv16_hdlr mmsimd_conv16_hdlr_t;
+
+struct mmsimd_conv32_hdlr;
+typedef struct mmsimd_conv32_hdlr mmsimd_conv32_hdlr_t;
+
+struct mmsimd_conv64_hdlr;
+typedef struct mmsimd_conv64_hdlr mmsimd_conv64_hdlr_t;
+
+typedef mmint_t(mmsimd_block_size_func_t)();
+
+typedef void(mmsimd_i8_func_t)(
+    const  int8_t* _a, const  int8_t* _b,  int8_t* _out, mmint_t _len);
+typedef void(mmsimd_u8_func_t)(
+    const uint8_t* _a, const uint8_t* _b, uint8_t* _out, mmint_t _len);
+typedef void(mmsimd_i8_scalar_func_t)(
+    const  int8_t* _a,  int8_t _b,  int8_t* _out, mmint_t _len);
+typedef void(mmsimd_u8_scalar_func_t)(
+    const uint8_t* _a, uint8_t _b, uint8_t* _out, mmint_t _len);
+
+typedef void(mmsimd_i16_func_t)(
+    const  int16_t* _a, const  int16_t* _b,  int16_t* _out, mmint_t _len);
+typedef void(mmsimd_u16_func_t)(
+    const uint16_t* _a, const uint16_t* _b, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_i32_func_t)(
+    const  int32_t* _a, const  int32_t* _b,  int32_t* _out, mmint_t _len);
+typedef void(mmsimd_u32_func_t)(
+    const uint32_t* _a, const uint32_t* _b, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_i64_func_t)(
+    const  int64_t* _a, const  int64_t* _b,  int64_t* _out, mmint_t _len);
+typedef void(mmsimd_u64_func_t)(
+    const uint64_t* _a, const uint64_t* _b, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_i16_scalar_func_t)(
+    const  int16_t* _a,  int16_t _b,  int16_t* _out, mmint_t _len);
+typedef void(mmsimd_u16_scalar_func_t)(
+    const uint16_t* _a, uint16_t _b, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_i32_scalar_func_t)(
+    const  int32_t* _a,  int32_t _b,  int32_t* _out, mmint_t _len);
+typedef void(mmsimd_u32_scalar_func_t)(
+    const uint32_t* _a, uint32_t _b, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_i64_scalar_func_t)(
+    const  int64_t* _a,  int64_t _b,  int64_t* _out, mmint_t _len);
+typedef void(mmsimd_u64_scalar_func_t)(
+    const uint64_t* _a, uint64_t _b, uint64_t* _out, mmint_t _len);
+    
+typedef void(mmsimd_f32_func_t)(
+    const  float* _a, const  float* _b,  float* _out, mmint_t _len);
+typedef void(mmsimd_f64_func_t)(
+    const double* _a, const double* _b, double* _out, mmint_t _len);
+typedef void(mmsimd_f32_scalar_func_t)(
+    const  float* _a,  float _b,  float* _out, mmint_t _len);
+typedef void(mmsimd_f64_scalar_func_t)(
+    const double* _a, double _b, double* _out, mmint_t _len);
+
+typedef void(mmsimd_i8_to_i16_func_t)(const int8_t* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_i8_to_u16_func_t)(const int8_t* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_i8_to_i32_func_t)(const int8_t* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_i8_to_u32_func_t)(const int8_t* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_i8_to_i64_func_t)(const int8_t* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_i8_to_u64_func_t)(const int8_t* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_i8_to_f32_func_t)(const int8_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_i8_to_f64_func_t)(const int8_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_u8_to_i16_func_t)(const uint8_t* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_u8_to_u16_func_t)(const uint8_t* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_u8_to_i32_func_t)(const uint8_t* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_u8_to_u32_func_t)(const uint8_t* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_u8_to_i64_func_t)(const uint8_t* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_u8_to_u64_func_t)(const uint8_t* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_u8_to_f32_func_t)(const uint8_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_u8_to_f64_func_t)(const uint8_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_i16_to_i8_func_t )(const int16_t* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_i16_to_u8_func_t )(const int16_t* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_i16_to_i32_func_t)(const int16_t* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_i16_to_u32_func_t)(const int16_t* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_i16_to_i64_func_t)(const int16_t* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_i16_to_u64_func_t)(const int16_t* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_i16_to_f32_func_t)(const int16_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_i16_to_f64_func_t)(const int16_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_u16_to_i8_func_t )(const uint16_t* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_u16_to_u8_func_t )(const uint16_t* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_u16_to_i32_func_t)(const uint16_t* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_u16_to_u32_func_t)(const uint16_t* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_u16_to_i64_func_t)(const uint16_t* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_u16_to_u64_func_t)(const uint16_t* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_u16_to_f32_func_t)(const uint16_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_u16_to_f64_func_t)(const uint16_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_i32_to_i8_func_t )(const int32_t* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_i32_to_u8_func_t )(const int32_t* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_i32_to_i16_func_t)(const int32_t* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_i32_to_u16_func_t)(const int32_t* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_i32_to_i64_func_t)(const int32_t* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_i32_to_u64_func_t)(const int32_t* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_i32_to_f32_func_t)(const int32_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_i32_to_f64_func_t)(const int32_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_u32_to_i8_func_t )(const uint32_t* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_u32_to_u8_func_t )(const uint32_t* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_u32_to_i16_func_t)(const uint32_t* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_u32_to_u16_func_t)(const uint32_t* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_u32_to_i64_func_t)(const uint32_t* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_u32_to_u64_func_t)(const uint32_t* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_u32_to_f32_func_t)(const uint32_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_u32_to_f64_func_t)(const uint32_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_i64_to_i8_func_t )(const int64_t* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_i64_to_u8_func_t )(const int64_t* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_i64_to_i16_func_t)(const int64_t* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_i64_to_u16_func_t)(const int64_t* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_i64_to_i32_func_t)(const int64_t* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_i64_to_u32_func_t)(const int64_t* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_i64_to_f32_func_t)(const int64_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_i64_to_f64_func_t)(const int64_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_u64_to_i8_func_t )(const uint64_t* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_u64_to_u8_func_t )(const uint64_t* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_u64_to_i16_func_t)(const uint64_t* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_u64_to_u16_func_t)(const uint64_t* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_u64_to_i32_func_t)(const uint64_t* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_u64_to_u32_func_t)(const uint64_t* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_u64_to_f32_func_t)(const uint64_t* _in, float*    _out, mmint_t _len);
+typedef void(mmsimd_u64_to_f64_func_t)(const uint64_t* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_f32_to_i8_func_t )(const float* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_f32_to_u8_func_t )(const float* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_f32_to_i16_func_t)(const float* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_f32_to_u16_func_t)(const float* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_f32_to_i32_func_t)(const float* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_f32_to_u32_func_t)(const float* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_f32_to_i64_func_t)(const float* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_f32_to_u64_func_t)(const float* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_f32_to_f64_func_t)(const float* _in, double*   _out, mmint_t _len);
+
+typedef void(mmsimd_f64_to_i8_func_t )(const double* _in, int8_t*   _out, mmint_t _len);
+typedef void(mmsimd_f64_to_u8_func_t )(const double* _in, uint8_t*  _out, mmint_t _len);
+typedef void(mmsimd_f64_to_i16_func_t)(const double* _in, int16_t*  _out, mmint_t _len);
+typedef void(mmsimd_f64_to_u16_func_t)(const double* _in, uint16_t* _out, mmint_t _len);
+typedef void(mmsimd_f64_to_i32_func_t)(const double* _in, int32_t*  _out, mmint_t _len);
+typedef void(mmsimd_f64_to_u32_func_t)(const double* _in, uint32_t* _out, mmint_t _len);
+typedef void(mmsimd_f64_to_i64_func_t)(const double* _in, int64_t*  _out, mmint_t _len);
+typedef void(mmsimd_f64_to_u64_func_t)(const double* _in, uint64_t* _out, mmint_t _len);
+typedef void(mmsimd_f64_to_f32_func_t)(const double* _in, float*    _out, mmint_t _len);
+
+MEME_EXTERN_C_SCOPE_ENDED
+#endif // !MEME_IMPL_SIMD_SIMD_FWD_H_INCLUDED
