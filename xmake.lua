f174e0d7c2bf5e2235206614fde47eaca6683fa6
FlowerFerry <flower_ferry@outlook.com>
2025-01-06 17:23:22 +0800

    -

--- xmake.lua
+++ xmake.lua
@@ -19,6 +19,22 @@ option("memestr_benchmark_enable")
     set_description("Enable benchmark of meme library")
 option_end()
 
+function check_c_compiler_flag(flag)
+    import("core.tool.compiler")
+
+    local test_file = path.join(os.tmpdir(), "check_c_compiler_flag_test.c")
+    local test_prog = path.join(os.tmpdir(), "check_c_compiler_flag_test")
+
+    io.writefile(test_file, "int main(int argc, char **argv) { return 0; }")
+
+    local ok = os.runv(compiler.features("cc"):binary(), {flag, "-o", testprog, testfile})
+
+    os.rm(test_file)
+    os.rm(test_prog)
+
+    return ok
+end
+
 -- stdc = "c11"
 -- set_languages(stdc)
 
@@ -37,6 +53,18 @@ target("meme_string")
         add_syslinks("rt")
         add_cxxflags("-fPIC", "-fexceptions")
         add_ldflags ("-fPIC", "-fexceptions")
+    end
+    if is_cc("msvc") ~= true then
+        local supports_avx2 = check_c_compiler_flag("-mavx2")
+        local supports_neon = check_c_compiler_flag("-mfpu=neon")
+
+        if supports_avx2 then
+            add_cflags("-mavx2")
+        end
+
+        if supports_neon then
+            add_cflags("-mfpu=neon")
+        end
     end
     set_kind("shared")
     set_symbols("hidden")
