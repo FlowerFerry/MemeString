5dbd8251abb7d6edacf0f397b53edb6a60bfed7b
FlowerFerry <flower_ferry@outlook.com>
2025-04-05 00:39:08 +0800

    -

--- include/mego/predef/architecture/sys390.h
+++ include/mego/predef/architecture/sys390.h
@@ -3,7 +3,7 @@
 
 #if !defined(MEGO_ARCH__STRING)
 
-#if #if defined(__s390__) || defined(__s390x__)
+#if defined(__s390__) || defined(__s390x__)
 #   define MEGO_ARCH__SYS390 MEGO__MAKE_VERSION_NUMBER(0, 0, 1)
 #   define MEGO_ARCH__STRING "System/390"
 #endif
