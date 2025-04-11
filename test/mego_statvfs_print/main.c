1ed8c32c6f0107f091e741fdc64a85af6b7bc150
FlowerFerry <flower_ferry@outlook.com>
2025-04-12 01:14:29 +0800

    -

--- test/mego_statvfs_print/main.c
+++ test/mego_statvfs_print/main.c
@@ -25,15 +25,18 @@ int main(int _argc, char *_argv[])
         printf("Failed to get file system information.\n");
     }
     else {
+        printf("statvfs.f_bsize: %llu\n", buf.f_bsize);
+        printf("statvfs.f_frsize: %llu\n", buf.f_frsize);
         printf("statvfs.f_blocks: %llu\n", buf.f_blocks);
         printf("statvfs.f_bfree: %llu\n", buf.f_bfree);
         printf("statvfs.f_bavail: %llu\n", buf.f_bavail);
-        printf("statvfs.f_frsize: %llu\n", buf.f_frsize);
+        printf("statvfs.f_files: %llu\n", buf.f_files);
+        printf("statvfs.f_ffree: %llu\n", buf.f_ffree);
+        printf("statvfs.f_favail: %llu\n", buf.f_favail);
         printf("statvfs.f_fsid: %llu\n", buf.f_fsid);
         printf("statvfs.f_flag: %u\n", buf.f_flag);
         printf("statvfs.f_namemax: %u\n", buf.f_namemax);
-        printf("statvfs.f_fstr: %s\n", buf.f_fstr);
-        
+
     }
 #endif
 
