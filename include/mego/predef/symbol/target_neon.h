637a9991280a35c42a16a127da314b5b9490712c
FlowerFerry <flower_ferry@outlook.com>
2025-08-14 11:17:21 +0800

    -

--- include/mego/predef/symbol/target_neon.h
+++ include/mego/predef/symbol/target_neon.h
@@ -1,4 +1,4 @@
-
+﻿
 #ifndef MEGO_PREDEF_SYMBOL_TARGET_NEON_H_INCLUDED
 #define MEGO_PREDEF_SYMBOL_TARGET_NEON_H_INCLUDED
 
@@ -6,7 +6,7 @@
 #include <mego/predef/compiler/gcc.h>
 #include <mego/predef/compiler/clang.h>
 
-#if MEGO_ARCH__ARM && !MEGO_ARCH__ARM64 && (MG_COMP__GCC_AVAIL || MG_COMP__CLANG_AVAIL)
+#if MEGO_ARCH__ARM && defined(__ARM_NEON) && !MEGO_ARCH__ARM64 && (MG_COMP__GCC_AVAIL || MG_COMP__CLANG_AVAIL)
 #define MEGO_SYMBOL__TARGET_NEON __attribute__((target("neon")))
 #else
 #define MEGO_SYMBOL__TARGET_NEON
